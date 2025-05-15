150fc584ca0eb67a6f55f745d5ce51a4da73a6da
Knevari <winresource000@gmail.com>
2025-05-14 21:05:14 -0300

    click handling, entity hovering, better movement, better debug, player range

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,38 @@
 import Alea from "alea";
 import { createNoise2D } from "simplex-noise";
 
+// images
+export const tilesetImg = new Image();
+tilesetImg.src = "/tilemap_packed.png";
+
+function loadAssets() {
+  const assets = [tilesetImg];
+
+  return new Promise((resolve) => {
+    let loaded = 0;
+
+    assets.forEach((asset) => {
+      asset.onload = () => {
+        loaded++;
+        if (loaded === assets.length) {
+          resolve(assets);
+        }
+      };
+    });
+  });
+}
+
+// canvas
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 // constants
 const TILE_SIZE = 16;
 const PLAYER_SIZE = TILE_SIZE * 0.8;
+const PLAYER_RANGE = 8;
 const CHUNK_SIZE = 8;
 const CHUNK_SIZE_IN_PIXELS = TILE_SIZE * CHUNK_SIZE;
 const SEED = 123;
@@ -30,6 +56,18 @@ function floor(value: number) {
   return Math.floor(value);
 }
 
+function ceil(value: number) {
+  return Math.ceil(value);
+}
+
+function round(value: number) {
+  return Math.round(value);
+}
+
+function distance(x1: number, y1: number, x2: number, y2: number) {
+  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
 function map(
   value: number,
   start1: number,
@@ -51,6 +89,35 @@ function constrain(value: number, low: number, high: number) {
   return Math.max(Math.min(value, high), low);
 }
 
+// helpers
+function isColliding(
+  x1: number,
+  y1: number,
+  w1: number,
+  h1: number,
+  x2: number,
+  y2: number,
+  w2: number,
+  h2: number,
+): boolean {
+  const left1 = x1 - w1 / 2;
+  const right1 = x1 + w1 / 2;
+  const top1 = y1 - h1 / 2;
+  const bottom1 = y1 + h1 / 2;
+
+  const left2 = x2 - w2 / 2;
+  const right2 = x2 + w2 / 2;
+  const top2 = y2 - h2 / 2;
+  const bottom2 = y2 + h2 / 2;
+
+  return !(
+    right1 <= left2 ||
+    left1 >= right2 ||
+    bottom1 <= top2 ||
+    top1 >= bottom2
+  );
+}
+
 // random
 const prng = Alea(SEED);
 const noise = createNoise2D(prng);
@@ -65,15 +132,78 @@ enum EntityType {
 }
 
 type Entity = {
+  id: number;
   type: EntityType;
   x: number;
   y: number;
+  w: number;
+  h: number;
+  health: {
+    current: number;
+    max: number;
+  };
 };
 
+let hoveredEntityId = -1;
+let entityId = 1;
 const entities: Entity[] = [];
 
-function createEntity(type: EntityType, worldX: number, worldY: number) {
-  entities.push({ type, x: worldX, y: worldY });
+function createEntity(
+  type: EntityType,
+  worldX: number,
+  worldY: number,
+  width: number,
+  height: number,
+) {
+  entities.push({
+    id: entityId++,
+    type,
+    x: worldX,
+    y: worldY,
+    w: width,
+    h: height,
+    health: {
+      current: 1,
+      max: 1,
+    },
+  });
+}
+
+function destroyEntity(entityId: number) {
+  const entityIndex = entities.findIndex((entity) => entity.id === entityId);
+  if (entityIndex !== -1) entities.splice(entityIndex, 1);
+}
+
+function canPlaceEntity(
+  worldX: number,
+  worldY: number,
+  width: number,
+  height: number,
+) {
+  const newEntityCenterX = worldX + (width * TILE_SIZE) / 2;
+  const newEntityCenterY = worldY + (width * TILE_SIZE) / 2;
+
+  for (const entity of entities) {
+    const entityCenterX = entity.x + (entity.w * TILE_SIZE) / 2;
+    const entityCenterY = entity.y + (entity.h * TILE_SIZE) / 2;
+
+    if (
+      isColliding(
+        newEntityCenterX,
+        newEntityCenterY,
+        width * TILE_SIZE,
+        height * TILE_SIZE,
+        entityCenterX,
+        entityCenterY,
+        entity.w * TILE_SIZE,
+        entity.h * TILE_SIZE,
+      )
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function spawnChunkEntities(chunkKey: ChunkKey) {
@@ -93,7 +223,9 @@ function spawnChunkEntities(chunkKey: ChunkKey) {
       );
 
       if (spawnProbability < 0.2) {
-        createEntity(EntityType.TREE, entityWorldX, entityWorldY);
+        if (canPlaceEntity(entityWorldX, entityWorldY, 1, 2)) {
+          createEntity(EntityType.TREE, entityWorldX, entityWorldY, 1, 2);
+        }
       }
     }
   }
@@ -104,14 +236,68 @@ function drawEntities() {
     const entity = entities[i];
     switch (entity.type) {
       case EntityType.TREE: {
+        ctx.save();
+
+        if (hoveredEntityId === entity.id) {
+          ctx.globalAlpha = 0.5;
+        }
+
         ctx.fillStyle = "#002C04";
         ctx.fillRect(
           entity.x - camera.x,
           entity.y - camera.y,
-          TILE_SIZE,
-          TILE_SIZE,
+          entity.w * TILE_SIZE,
+          entity.h * TILE_SIZE,
         );
+        ctx.restore();
       }
+    }
+  }
+}
+
+function drawEntitiesCenterpoints() {
+  for (const entity of entities) {
+    const entityCenterX = entity.x + (entity.w * TILE_SIZE) / 2;
+    const entityCenterY = entity.y + (entity.h * TILE_SIZE) / 2;
+
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.arc(
+      entityCenterX - camera.x,
+      entityCenterY - camera.y,
+      2,
+      0,
+      Math.PI * 2,
+    );
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+function getEntityAtWorldPosition(worldX: number, worldY: number) {
+  for (const entity of entities) {
+    const { x: entityX, y: entityY, w: entityWidth, h: entityHeight } = entity;
+    if (
+      worldX >= entityX &&
+      worldX <= entityX + entityWidth * TILE_SIZE &&
+      worldY >= entityY &&
+      worldY <= entityY + entityHeight * TILE_SIZE
+    ) {
+      return entity;
+    }
+  }
+  return null;
+}
+
+function handleEntityClick(entity: Entity) {
+  switch (entity.type) {
+    case EntityType.TREE: {
+      // destroy tree, drop item
+      entity.health.current -= 1;
+      if (entity.health.current <= 0) {
+        destroyEntity(entity.id);
+      }
+      console.log("Clicked on a tree at", entity.x, entity.y);
     }
   }
 }
@@ -174,14 +360,19 @@ function getChunkPositionFromKey(chunkKey: ChunkKey) {
   return chunkKey.split(",").map(Number);
 }
 
-function getChunkPositionFromWorldPosition(screenX: number, screenY: number) {
-  const chunkX = floor(screenX / CHUNK_SIZE_IN_PIXELS);
-  const chunkY = floor(screenY / CHUNK_SIZE_IN_PIXELS);
+function getChunkPositionFromWorldPosition(worldX: number, worldY: number) {
+  const chunkX = floor(worldX / CHUNK_SIZE_IN_PIXELS);
+  const chunkY = floor(worldY / CHUNK_SIZE_IN_PIXELS);
   return [chunkX, chunkY];
 }
 
-function getChunkKeyFromWorldPosition(screenX: number, screenY: number) {
-  const [chunkX, chunkY] = getChunkPositionFromWorldPosition(screenX, screenY);
+function getChunkFromWorldPosition(worldX: number, worldY: number) {
+  const [chunkX, chunkY] = getChunkPositionFromWorldPosition(worldX, worldY);
+  return chunks.get(createChunkKey(chunkX, chunkY));
+}
+
+function getChunkKeyFromWorldPosition(worldX: number, worldY: number) {
+  const [chunkX, chunkY] = getChunkPositionFromWorldPosition(worldX, worldY);
   return createChunkKey(chunkX, chunkY);
 }
 
@@ -216,10 +407,10 @@ function generateChunksAround(screenX: number, screenY: number) {
   const [chunkX, chunkY] = getChunkPositionFromWorldPosition(screenX, screenY);
 
   const neighbors: ChunkKey[] = [];
-  const n = 5;
+  const n = 3;
 
-  for (let dx = floor(-n / 2); dx < n / 2; dx++) {
-    for (let dy = floor(-n / 2); dy < n / 2; dy++) {
+  for (let dx = floor(-n / 2); dx < ceil(n / 2); dx++) {
+    for (let dy = floor(-n / 2); dy < ceil(n / 2); dy++) {
       if (dx === 0 && dy === 0) continue;
       neighbors.push(createChunkKey(chunkX + dx, chunkY + dy));
     }
@@ -234,25 +425,90 @@ function generateChunksAround(screenX: number, screenY: number) {
   }
 }
 
-// cursor
+function getChunkTileIndexFromWorldPosition(worldX: number, worldY: number) {
+  const chunkKey = getChunkKeyFromWorldPosition(worldX, worldY);
+  const [chunkTopLeftX, chunkTopLeftY] = getChunkTopLeftCorner(chunkKey);
+
+  const tileX = floor((worldX - chunkTopLeftX) / TILE_SIZE);
+  const tileY = floor((worldY - chunkTopLeftY) / TILE_SIZE);
+
+  return [tileX, tileY];
+}
+
+// cursor and interaction
 const cursor = {
   x: 0,
   y: 0,
-  hoveringEntity: false,
+  size: 4,
 };
 
 function drawCursor() {
   ctx.fillStyle = "#AC46CB";
   ctx.beginPath();
-  ctx.arc(cursor.x, cursor.y, 8, 0, Math.PI * 2);
+  ctx.arc(cursor.x, cursor.y, cursor.size, 0, Math.PI * 2);
   ctx.fill();
   ctx.closePath();
 }
 
 canvas.addEventListener("mousemove", (event) => {
   const { top, left } = canvas.getBoundingClientRect();
-  cursor.x += (event.clientX - left - cursor.x) * 0.1;
-  cursor.y += (event.clientY - top - cursor.y) * 0.1;
+  cursor.x = event.clientX - left;
+  cursor.y = event.clientY - top;
+
+  let hoveringAnyEntity = false;
+
+  // Verify if its hovering any entities
+  entities.forEach((entity) => {
+    const entityCenterX = entity.x + (entity.w * TILE_SIZE) / 2;
+    const entityCenterY = entity.y + (entity.h * TILE_SIZE) / 2;
+
+    const hovering = isColliding(
+      cursor.x + camera.x,
+      cursor.y + camera.y,
+      cursor.size,
+      cursor.size,
+      entityCenterX,
+      entityCenterY,
+      entity.w * TILE_SIZE,
+      entity.h * TILE_SIZE,
+    );
+
+    if (hovering) {
+      hoveringAnyEntity = true;
+      hoveredEntityId = entity.id;
+    }
+  });
+
+  if (hoveringAnyEntity) {
+    document.body.style.cursor = "pointer";
+  } else if (!hoveringAnyEntity && document.body.style.cursor === "pointer") {
+    document.body.style.cursor = "auto";
+  }
+});
+
+canvas.addEventListener("mousedown", (event) => {
+  const { top, left } = canvas.getBoundingClientRect();
+  const clickX = event.clientX - left;
+  const clickY = event.clientY - top;
+
+  const clickWorldX = clickX + camera.x;
+  const clickWorldY = clickY + camera.y;
+
+  // Verify if the click hit something
+  const clickedEntity = getEntityAtWorldPosition(clickWorldX, clickWorldY);
+
+  if (clickedEntity) {
+    const distanceFromPlayer = distance(
+      player.x,
+      player.y,
+      clickedEntity.x + (clickedEntity.w * TILE_SIZE) / 2,
+      clickedEntity.y + (clickedEntity.h * TILE_SIZE) / 2,
+    );
+
+    if (distanceFromPlayer < PLAYER_RANGE * TILE_SIZE) {
+      handleEntityClick(clickedEntity);
+    }
+  }
 });
 
 // player
@@ -285,12 +541,69 @@ function spawnPlayer() {
 
 function drawPlayer() {
   ctx.fillStyle = "#1e2328";
-  ctx.fillRect(
+  ctx.beginPath();
+  ctx.arc(
     player.x - camera.x,
     player.y - camera.y,
-    PLAYER_SIZE,
-    PLAYER_SIZE,
+    PLAYER_SIZE / 2,
+    0,
+    Math.PI * 2,
   );
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPlayerRange() {
+  ctx.strokeStyle = "#F5F5F5";
+  ctx.beginPath();
+  ctx.arc(
+    player.x - camera.x,
+    player.y - camera.y,
+    PLAYER_RANGE * TILE_SIZE,
+    0,
+    Math.PI * 2,
+  );
+  ctx.stroke();
+  ctx.closePath();
+}
+
+function playerCanMoveThere(worldX: number, worldY: number) {
+  const chunk = getChunkFromWorldPosition(worldX, worldY);
+
+  if (!chunk) return false;
+
+  const [chunkTileX, chunkTileY] = getChunkTileIndexFromWorldPosition(
+    worldX,
+    worldY,
+  );
+
+  // Verify if tile is walkable
+  if (chunk[chunkTileX][chunkTileY] !== Tile.GRASS) {
+    return false;
+  }
+
+  // Verify if there is any entity blocking movement
+  for (const entity of entities) {
+    const entityCenterX = entity.x + (entity.w * TILE_SIZE) / 2;
+    const entityCenterY = entity.y + (entity.h * TILE_SIZE) / 2;
+
+    const entityCollides = isColliding(
+      worldX,
+      worldY,
+      PLAYER_SIZE,
+      PLAYER_SIZE,
+      entityCenterX,
+      entityCenterY,
+      entity.w * TILE_SIZE,
+      entity.h * TILE_SIZE,
+    );
+
+    if (entityCollides) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 function movePlayer(deltaTime: number) {
@@ -301,8 +614,23 @@ function movePlayer(deltaTime: number) {
   if (pressedKeys.has("a")) direction.x -= 1;
   if (pressedKeys.has("d")) direction.x += 1;
 
-  player.x += player.speed * direction.x * deltaTime;
-  player.y += player.speed * direction.y * deltaTime;
+  const length = Math.hypot(direction.x, direction.y);
+  if (length > 0) {
+    direction.x /= length;
+    direction.y /= length;
+  }
+
+  const nextX = player.x + player.speed * direction.x * deltaTime;
+  const nextY = player.y + player.speed * direction.y * deltaTime;
+
+  if (playerCanMoveThere(nextX, nextY)) {
+    player.x = nextX;
+    player.y = nextY;
+  } else if (playerCanMoveThere(nextX, player.y)) {
+    player.x = nextX;
+  } else if (playerCanMoveThere(player.x, nextY)) {
+    player.y = nextY;
+  }
 }
 
 // camera
@@ -325,10 +653,14 @@ function clearBackground() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
-function main() {
+async function main() {
+  await loadAssets();
+
   createChunk(0, 0);
+  generateChunksAround(player.x, player.y);
 
   spawnPlayer();
+
   requestAnimationFrame(update);
 }
 
@@ -341,18 +673,19 @@ function update(now: number) {
 
   clearBackground();
 
-  generateChunksAround(player.x, player.y);
-
+  // generateChunksAround(player.x, player.y);
   movePlayer(deltaTime);
   moveCamera(deltaTime);
 
   drawChunks();
   drawPlayer();
   drawEntities();
-  drawCursor();
 
   if (debugModeEnabled) {
+    drawPlayerRange();
     highlightChunks();
+    drawEntitiesCenterpoints();
+    drawCursor();
   }
 
   requestAnimationFrame(update);
